f8ae5bade629094cf8eadb63aacd04d051027f98
Mats Fjellner <mats@matsfjellner.com>
2020-07-27 21:51:28 -0500

    Updates

---
+++ precache-manifest.29acd151cd61a3e0707fa91b743c877a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ad659d19bdf640549854c04edbcc87",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "7455247cbc016af5ffdc",
+    "url": "/ui/static/css/main.4e6b9790.chunk.css"
+  },
+  {
+    "revision": "10877639d29209de6580",
+    "url": "/ui/static/js/2.1a0bde38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ui/static/js/2.1a0bde38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7455247cbc016af5ffdc",
+    "url": "/ui/static/js/main.8aea4052.chunk.js"
+  },
+  {
+    "revision": "701cd08a73fe7a8a57fd",
+    "url": "/ui/static/js/runtime-main.1c71bcea.js"
+  },
+  {
+    "revision": "453ee82f6c3281d0dd79762d5c3f9323",
+    "url": "/ui/static/media/Lato.453ee82f.woff"
+  },
+  {
+    "revision": "3210acb0ae1bf00aac17f59192c0d7a9",
+    "url": "/ui/static/media/add-name-bg.3210acb0.jpg"
+  },
+  {
+    "revision": "c7362a40d5cf4d62a93a61899ab1e8e4",
+    "url": "/ui/static/media/francis2.c7362a40.jpg"
+  }
+]);
